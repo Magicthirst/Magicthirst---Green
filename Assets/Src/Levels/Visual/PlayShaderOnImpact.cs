391b08343d376553578b02b6fe54d35261e9ab26
yoloroy <slart2108@gmail.com>
2026-06-30 12:17:29 +0300

    :lipstick: add pushing shotgun shoot visual effect

---
+++ Assets/Src/Levels/Visual/PlayShaderOnImpact.cs
@@ -0,0 +1,57 @@
+using System;
+using Levels.Directorship;
+using Levels.Extensions;
+using Levels.IntentsImpacts;
+using UnityEngine;
+using Util;
+using VContainer;
+
+namespace Levels.Visual
+{
+    [RequireComponent(typeof(Renderer))]
+    public class PlayShaderOnImpact : LevelBehaviour
+    {
+        private static readonly int StartTime = Shader.PropertyToID("_StartTime");
+
+        protected override LevelActivityMask _LifecycleMask => (LevelActivityMask)activity;
+
+        [SerializeField] private EditorLevelActivityMask activity;
+
+        [SubtypeProperty(typeof(IImpact))]
+        [SerializeField]
+        private string impactType;
+
+        private Renderer _renderer;
+        private MaterialPropertyBlock _properties;
+
+        private IImpactConsumer _consumer;
+
+        [Inject]
+        private void Consumer(IObjectResolver resolver)
+        {
+            var tImpact = Type.GetType(impactType);
+            var consumerType = typeof(IImpactConsumer<>).MakeGenericType(tImpact);
+            _consumer = (IImpactConsumer) resolver.Resolve(consumerType);
+        }
+
+        private void Awake()
+        {
+            _renderer = GetComponent<Renderer>();
+            _properties = new MaterialPropertyBlock();
+            _renderer.GetPropertyBlock(_properties);
+        }
+
+        protected override void DidEnabled() => _consumer.Impacted += Run;
+
+        private void Run()
+        {
+            Debug.Log("_StartTime = LevelDirector.GameplayTime");
+            _renderer.UpdatePropertyBlock(_properties, b =>
+            {
+                b.SetFloat(StartTime, LevelDirector.GameplayTime);
+            });
+        }
+
+        protected override void DidDisabled() => _consumer.Impacted -= Run;
+    }
+}
